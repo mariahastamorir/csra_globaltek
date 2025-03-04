94befe9c75580db50b79dada269d6769c6ef622b
mariahastamorir <maria.hastamorir@globalteksecurity.com>
2025-03-04 17:48:35 -0500

    modificacion 2

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     //Importar Reactive Form Module para  trabajar los fomularios
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Aquí puedes añadir la lógica para manejar el inicio de sesión
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+  }
 
 }
