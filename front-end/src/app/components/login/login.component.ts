c34fcf5ee9a3e257afd4d7c89460f627e72af986
mariahastamorir <maria.hastamorir@globalteksecurity.com>
2025-03-06 15:00:26 -0500

    Modificación login css y html

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], // Validación de email
+      password: ['', [Validators.required, Validators.minLength(6)]], // Validación de contraseña
+    });
+  }
+  
+  usuario: string = '';
+  contrasena: string = '';
+  showPassword: boolean = false;
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 
   onSubmit() {
-    // Aquí puedes añadir la lógica para manejar el inicio de sesión
-    console.log('Usuario:', this.username);
-    console.log('Contraseña:', this.password);
+    console.log('Usuario:', this.usuario);
+    console.log('Contraseña:', this.contrasena);
   }
 
 }
