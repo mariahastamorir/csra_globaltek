729f8925cc5d9b163d9e92df5baea8e5acae4c11
CarmenHerreraB <carmen.herrera@globalteksecurity.com>
2025-03-25 14:32:47 -0500

    h

--- backend/basededatos/models/tipodocumento.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TipoDocumento(models.Model):
-    nombre = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.nombre
